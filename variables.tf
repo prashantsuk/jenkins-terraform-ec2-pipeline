a13eeaabefbb81750f7f1a4b23591fffdd591abf
prashantsuk <42904978+prashantsuk@users.noreply.github.com>
2023-09-08 14:44:50 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -21,7 +21,7 @@ variable "security_group" {
 
 variable "tag_name" {
   description = "Tag Name of for Ec2 instance"
-  default     = "my-ec2-instance"
+  default     = "test-ec2-instance"
 }
 
 variable "ami_id" {
