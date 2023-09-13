280c976dda45b81e785fb03432709cb8654c8dd8
prashantsuk <42904978+prashantsuk@users.noreply.github.com>
2023-09-13 19:28:44 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -16,7 +16,7 @@ variable "instance_type" {
 variable "security_group" {
   description = "Name of security group"
   //default     = "my-jenkins-security-group"
-  default  = "launch-wizard-8-5"
+  default  = "launch-wizard-8-6"
 }
 
 variable "tag_name" {
